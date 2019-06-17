36f13bad42266b5413e483d3e0a7470f3a62d220
zven21 <andzven@gmail.com>
2019-06-17 17:15:49 +0800

    mix hex.publish

--- mix.exs
+++ mix.exs
@@ -1,11 +1,17 @@
 defmodule Ancestry.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/zven21/ancestry"
+
   def project do
     [
       app: :ancestry,
-      version: "0.1.0",
+      version: @version,
+      description: "The tree structure implementations for Ecto.",
       elixir: "~> 1.8",
+      package: package(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +27,21 @@ defmodule Ancestry.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["zven21"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "priv", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
